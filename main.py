13559eaf91a461955accab04c37e45b4cea88bf4
math-zhuxy <235934429@qq.com>
2025-01-10 21:28:37 +0800

    增加了一点prompt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import ai_chat
 if __name__ == "__main__":
     client = ai_chat.AIChat()
+    print("按下回车健开始游戏")
     while True:
         print("--------------------------------------")
         user_input = input()
         print(client.communicate(user_input))
-        print("--------------------------------------")
+        print("")
