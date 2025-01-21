b1fb8a41533ef6f188c39f2d2f9d602eb2cf6200
math-zhuxy <235934429@qq.com>
2025-01-21 21:46:23 +0800

    初始化前端页面

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     server = server.Server()
     
     # 启动 FastAPI 应用
-    uvicorn.run(server.app, host="0.0.0.0", port=8000)
+    uvicorn.run(server.app, host="127.0.0.1", port=8000)
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+from tempfile import template
+import fastapi.staticfiles
+import fastapi.templating
 import pydantic
 import fastapi
 import ai_chat
@@ -9,7 +12,14 @@ class Item(pydantic.BaseModel):
 class Server:
     def __init__(self):
         self.app = fastapi.FastAPI()
+        self.app.mount("/static", fastapi.staticfiles.StaticFiles(directory="public/static"), name="static")
+        template = fastapi.templating.Jinja2Templates(directory="public")
         self.chat_model = ai_chat.AIChat()
+
+        @self.app.get("/")
+        async def index(request: fastapi.Request):
+            return template.TemplateResponse("main.html", {"request": request})
+
         @self.app.get("/ai_chat")
         async def process_message(input: Item):
             return self.chat_model.communicate(input.msg)
